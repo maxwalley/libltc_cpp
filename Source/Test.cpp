5202c1d8d025d798cdaa0eb1e1559f62c98a4e88
Max Walley <max.walley1@btinternet.com>
2022-07-12 23:06:37 +0100

    Added an xcode project with a basic test file

---
+++ Source/Test.cpp
@@ -0,0 +1,13 @@
+//
+//  Test.cpp
+//  libltc_cpp
+//
+//  Created by Max Walley on 12/07/2022.
+//
+
+#include "Test.hpp"
+
+int multiply(int first, int second)
+{
+    return first * second;
+}
---
+++ Source/Test.hpp
@@ -0,0 +1,15 @@
+//
+//  Test.hpp
+//  libltc_cpp
+//
+//  Created by Max Walley on 12/07/2022.
+//
+
+#ifndef Test_hpp
+#define Test_hpp
+
+#include <stdio.h>
+
+int multiply(int first, int second);
+
+#endif /* Test_hpp */
