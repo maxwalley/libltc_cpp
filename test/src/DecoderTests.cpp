5bd061c5e1cf709f7be31b6dbcda2b672f92df6a
Max Walley <max.walley1@btinternet.com>
2024-02-28 15:13:48 +0000

    Updated decoder tests

--- test/src/DecoderTests.cpp
+++ test/src/DecoderTests.cpp
@@ -1,34 +1,70 @@
 #include "gtest/gtest.h"
 #include "TestState.h"
 
-TEST(DecoderTests, DecodesSingleValidFrame)
+//Test that a single valid frame decodes correctly
+TEST(DecoderTests, DecodesValidSingleFrame)
 {
     std::cout << testFilesDirectory << std::endl;
-    
+
     ASSERT_EQ(4, 5);
 }
 
-TEST(DecoderTests, DecodesSingleInvalidFrame)
+//Test that a single frame of random bytes decodes incorectly and reports an error
+TEST(DecoderTests, DecodesInvalidSingleFrame)
 {
 
 }
 
-TEST(DecoderTests, TestMultiFrame)
+//Test that decoding a stream of multiple valid sequential frames performs correctly and reports no errors
+TEST(DecoderTests, DecodesValidMultiFrame)
 {
 
 }
 
-TEST(DecoderTests, TestDecodedFrameBufferSize)
+//Test that decoding a stream of multiple valid frames that are not in order reports a warning
+TEST(DecoderTests, DecodesNonSequentialMultiFrame)
+{
+    
+}
+
+//Test that decoding works at multiple sample rates
+TEST(DecoderTests, DecodesMultiSampleRate)
 {
 
 }
 
-TEST(DecoderTests, TestSampleRate)
+//Test that decoding a stream that is not at the specified sample rate reports an error
+TEST(DecoderTests, DecodesWrongSampleRate)
+{
+    
+}
+
+//Test that decoding works at multiple frame rates
+TEST(DecoderTests, DecodesMultiFrameRate)
 {
 
 }
 
-TEST(DecoderTests, TestFrameRate)
+//Test that decoding a stream that is not at the specified frame rate reports an error
+TEST(DecoderTests, DecodesWrongFrameRate)
 {
+    
+}
 
+//Test that user bits are maintained through decoding
+TEST(DecoderTests, DecodesUserBits)
+{
+    
+}
+
+//Test that reverse frames can be correctly decoded
+TEST(DecoderTests, DecodesReverseFrames)
+{
+    
+}
+
+//Test that reverse frames not in order reports a warning
+TEST(DecoderTests, DecodesReverseFramesNonSequential)
+{
+    
 }
